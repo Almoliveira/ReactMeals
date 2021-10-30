b848bc84d7400b3e4188decddb49628df316dad0
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-10-30 13:11:23 -0300

    Working with Refs.

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,20 @@
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
 
+  const addTOCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
+  };
   return (
     <li className={classes.meal}>
       <div>
@@ -12,7 +23,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm props={props.id}/>
+        <MealItemForm props={props.id} onAddToCart={addTOCartHandler}/>
       </div>
     </li>
   );
