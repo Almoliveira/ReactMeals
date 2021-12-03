6a5e8ba8da1fdc2fb16c68015d35dd32d87dc4d9
André Luís Martins de Oliveira <almoliveira.info@gmail.com>
2021-12-03 04:09:36 -0300

    Added HTTP Requests

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -65,6 +65,10 @@ const cartReducer = (state, action) => {
       }
   }
 
+  if(action.type === 'CLEAR') {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -82,11 +86,15 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({typee: 'CLEAR'});
+  }
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
 
   return (
